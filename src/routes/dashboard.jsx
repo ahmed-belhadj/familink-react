ba63d8f0e0265b7694767daae36f441bd8a740c1
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-02-24 12:54:54 -0600

    Update dashboard.jsx

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -8,18 +8,11 @@ import UserPage from "views/UserPage/UserPage.jsx";
 
 var dashRoutes = [
   {
-    path: "/dashboard/dashboard",
-    name: "Dashboard",
-    icon: "nc-icon nc-bank",
-    component: Dashboard
+    path: "/dashboard/user-page",
+    name: "Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage
   },
-  {
-    path: "/dashboard/icons",
-    name: "Icons",
-    icon: "nc-icon nc-diamond",
-    component: Icons
-  },
-  { path: "/dashboard/maps", name: "Maps", icon: "nc-icon nc-pin-3", component: Maps },
   {
     path: "/dashboard/notifications",
     name: "Notifications",
@@ -27,22 +20,54 @@ var dashRoutes = [
     component: Notifications
   },
   {
-    path: "/dashboard/user-page",
-    name: "User Profile",
-    icon: "nc-icon nc-single-02",
-    component: UserPage
+    path: "/dashboard/patient-demo",
+    name: "Patient Demo",
+    icon: "nc-icon nc-alert-circle-i",
+    component: Dashboard
   },
+  {
+    path: "/dashboard/doctor-demo",
+    name: "Doctor Demo",
+    icon: "nc-icon nc-ambulance",
+    component: Dashboard
+  },
+
+  // {
+  //   path: "/dashboard/dashboard",
+  //   name: "Dashboard",
+  //   icon: "nc-icon nc-bank",
+  //   component: Dashboard
+  // },
+  {
+    path: "/dashboard/icons",
+    name: "Icons",
+    icon: "nc-icon nc-diamond",
+    component: Icons
+  },
+  // { path: "/dashboard/maps", name: "Maps", icon: "nc-icon nc-pin-3", component: Maps },
+  // {
+  //   path: "/dashboard/notifications",
+  //   name: "Notifications",
+  //   icon: "nc-icon nc-bell-55",
+  //   component: Notifications
+  // },
+  // {
+  //   path: "/dashboard/user-page",
+  //   name: "User Profile",
+  //   icon: "nc-icon nc-single-02",
+  //   component: UserPage
+  // },
   {
     path: "/dashboard/tables",
     name: "Table List",
     icon: "nc-icon nc-tile-56",
     component: TableList
   },
-  {
-    path: "/dashboard/typography",
-    name: "Typography",
-    icon: "nc-icon nc-caps-small",
-    component: Typography
-  },
+  // {
+  //   path: "/dashboard/typography",
+  //   name: "Typography",
+  //   icon: "nc-icon nc-caps-small",
+  //   component: Typography
+  // },
 ];
 export default dashRoutes;
