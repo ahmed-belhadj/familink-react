eb8b9e08ec9a146120c7d8088fd439c197823d19
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-02-23 23:04:16 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,69 @@
-
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Button } from 'reactstrap';
+import React, {Component} from 'react';
+import {Navbar, NavbarBrand, Button} from 'reactstrap';
+import Landing from "layouts/Landing/Landing";
 
 class App extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`);
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
-  componentDidMount() {
-    const { renewSession } = this.props.auth;
-
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-      renewSession();
+    goTo(route) {
+        this.props.history.replace(`/${route}`);
     }
-  }
 
-  render() {
-    const { isAuthenticated } = this.props.auth;
+    login() {
+        this.props.auth.login();
+    }
 
-    return (
-      <div>
-        <Navbar color="dark" light expand="md">
-            <NavbarBrand href="">Auth0 - React</NavbarBrand>
-            <Button
-              bsstyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
-            {
-              !isAuthenticated() && (
-                  <Button
-                    bsstyle="primary"
-                    className="btn-margin"
-                    onClick={this.login.bind(this)}
-                  >
-                    Log In
-                  </Button>
-                )
-            }
-            {
-              isAuthenticated() && (
-                  <Button
-                    bsStyle="primary"
-                    className="btn-margin"
-                    onClick={this.logout.bind(this)}
-                  >
-                    Log Out
-                  </Button>
-                )
-            }
-        </Navbar>
-      </div>
-    );
-  }
+    logout() {
+        this.props.auth.logout();
+    }
+
+    componentDidMount() {
+        const {renewSession} = this.props.auth;
+
+        if (localStorage.getItem('isLoggedIn') === 'true') {
+            renewSession();
+        }
+    }
+
+    render() {
+        const {isAuthenticated} = this.props.auth;
+
+        return (
+            <div>
+                <Navbar color="dark" light expand="md">
+                    <NavbarBrand href="">Auth0 - React</NavbarBrand>
+                    <Button
+                        bsstyle="primary"
+                        className="btn-margin"
+                        onClick={this.goTo.bind(this, 'home')}
+                    >
+                        Home
+                    </Button>
+                    {
+                        !isAuthenticated() && (
+                            <Button
+                                bsstyle="primary"
+                                className="btn-margin"
+                                onClick={this.login.bind(this)}
+                            >
+                                Log In
+                            </Button>
+                        )
+                    }
+                    {
+                        isAuthenticated() && (
+                            <Button
+                                bsStyle="primary"
+                                className="btn-margin"
+                                onClick={this.logout.bind(this)}
+                            >
+                                Log Out
+                            </Button>
+                        )
+                    }
+                </Navbar>
+                <Landing />
+            </div>
+        );
+    }
 }
 
 export default App;
