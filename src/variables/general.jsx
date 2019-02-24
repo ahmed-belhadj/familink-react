90e9ba4bd4683c4f9537ab52e9a260e53312efe7
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-02-24 14:24:14 -0600

    Update general.jsx

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -2,35 +2,51 @@
 // // // table head data and table body data for Tables view
 // #############################
 
-const thead = ["Name", "Country", "City", "Salary"];
+const thead = ["Name", "Relationship", "Living", "Disease", "Age of Onset", "Age of Death"];
 const tbody = [
-  {
-    className: "table-success",
-    data: ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"]
-  },
-  {
-    className: "",
-    data: ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"]
-  },
-  {
-    className: "table-info",
-    data: ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"]
-  },
-  {
-    className: "",
-    data: ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"]
-  },
-  {
-    className: "table-danger",
-    data: ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"]
-  },
-  { className: "", data: ["Mason Porter", "Chile", "Gloucester", "$78,615"] },
-  {
-    className: "table-warning",
-    data: ["Jon Porter", "Portugal", "Gloucester", "$98,615"]
-  }
+    {
+        className: "table-success",
+        data: ["Sarah", "Self", "Yes", "Diabetes: Type 2","3",""]
+    },
+    {
+        className: "",
+        data: ["Charles", "Brother", "Yes", "Diabetes: Type 1","44",""]
+    },
+    {
+        className: "table-info",
+        data: ["John", "Father", "No", "Cancer: Bone Cancer","58","32"]
+    },
+    {
+        className: "",
+        data: ["Sherry", "Mother", "Yes", "Hypertension", "70",""]
+    },
+    {
+        className: "table-danger",
+        data: ["Sam", "Son", "No", "Cancer: Leukemia","4","7"]
+    },
+    {
+        className: "table-warning",
+        data: ["Gerald", "Paternal Grandfather", "No", "Clotting Disorder: Deep Vein Thrombosis","71","71"]
+    },
+    {
+        className: "table-warning",
+        data: ["Edna", "Paternal Grandmother", "No", "Cancer: Stomach","77","78"]
+    }, {
+        className: "table-warning",
+        data: ["Caroline", "Maternal Grandfather", "Yes", "Heart Attack","51","51"]
+    }, {
+        className: "table-warning",
+        data: ["Fred", "Maternal Grandfather", "Yes", "Cancer: Thyroid","60",""]
+    }, {
+        className: "table-warning",
+        data: ["Sally", "Maternal Aunt", "Yes", "", "", ""]
+    },
+    {
+        className: "table-warning",
+        data: ["George", "Maternal Cousin", "Yes", "", "", ""]
+    },
 ];
 
 // data for <thead> of table in TableList view
 // data for <tbody> of table in TableList view
-export { thead, tbody };
+export {thead, tbody};
