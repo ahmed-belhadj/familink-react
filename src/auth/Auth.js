b853d5c22fbc8f4267ffe6ebbc47918f3f01cedd
Tyler Hughes <tyler@tylerhughes.info>
2019-02-24 10:38:53 -0600

    Fixed issue where login button wasn't redirecting to Auth0 login page

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,129 +1,134 @@
 import auth0 from 'auth0-js';
+import { AUTH_CONFIG } from './auth0-variables';
 import history from '../history';
 
 export default class Auth {
     accessToken;
     idToken;
     expiresAt;
+    userProfile;
     tokenRenewalTimeout;
 
+    auth0 = new auth0.WebAuth( {
+        domain: AUTH_CONFIG.domain,
+        clientID: AUTH_CONFIG.clientId,
+        redirectUri: AUTH_CONFIG.callbackUrl,
+        responseType: 'token id_token',
+        scope: 'openid profile'
+    } );
+
     constructor() {
-        const domain = 'familink.auth0.com/',
-            clientId = 'uy5FOqsRDTpLBj20zosqyBB16Cr9Mmvr';
-
-        this.auth0 = new auth0.WebAuth({
-            domain: domain,
-            clientID: clientId,
-            redirectUri: 'http://localhost:3000/callback',
-            responseType: 'token id_token',
-            scope: 'openid'
-        });
-
-        this.getProfile = this.getProfile.bind(this);
-        this.handleAuthentication = this.handleAuthentication.bind(this);
-        this.isAuthenticated = this.isAuthenticated.bind(this);
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this);
-        this.setSession = this.setSession.bind(this);
+        this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.isAuthenticated = this.isAuthenticated.bind(this);
         this.getAccessToken = this.getAccessToken.bind(this);
         this.getIdToken = this.getIdToken.bind(this);
         this.renewSession = this.renewSession.bind(this);
+        this.getProfile = this.getProfile.bind(this);
+        this.getExpiryDate = this.getExpiryDate.bind(this);
+        this.scheduleRenewal();
+    }
+
+    login() {
+        this.auth0.authorize();
+    }
+
+    handleAuthentication() {
+        this.auth0.parseHash( ( err, authResult ) => {
+            if ( authResult && authResult.accessToken && authResult.idToken ) {
+                this.setSession(authResult);
+            } else if ( err ) {
+                history.replace('/home');
+                console.log( err );
+                alert(`Error: ${err.error}. Check the console for further details.`);
+            }
+        } );
+    }
+
+    getAccessToken() {
+        return this.accessToken;
+    }
+
+    getIdToken() {
+        return this.idToken;
+    }
+
+    setSession(authResult) {
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+
+        // Set the time that the access token will expire at
+        let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+        this.accessToken = authResult.accessToken;
+        this.idToken = authResult.idToken;
+        this.expiresAt = expiresAt;
 
         // schedule a token renewal
         this.scheduleRenewal();
-  }
 
-  getProfile() {
-    return this.profile;
-  }
-
-  handleAuthentication() {
-    this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-      } else if (err) {
+        // navigate to the home route
         history.replace('/home');
-        console.log(err);
-        alert(`Error: ${err.error}. Check the console for further details.`);
-      }
-    });
-  }
-
-  getAccessToken() {
-    return this.accessToken;
-  }
-
-  getIdToken() {
-    return this.idToken;
-  }
-
-  setSession(authResult) {
-    // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
-
-    // Set the time that the access token will expire at
-    let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
-    this.accessToken = authResult.accessToken;
-    this.idToken = authResult.idToken;
-    this.expiresAt = expiresAt;
-
-    // schedule a token renewal
-    this.scheduleRenewal();
-
-    // navigate to the home route
-    history.replace('/');
-  }
-
-  renewSession() {
-    this.auth0.checkSession({}, (err, authResult) => {
-       if (authResult && authResult.accessToken && authResult.idToken) {
-         this.setSession(authResult);
-       } else if (err) {
-         this.logout();
-         console.log(err);
-         alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
-       }
-    });
-  }
-
-  scheduleRenewal() {
-    let expiresAt = this.expiresAt;
-    const timeout = expiresAt - Date.now();
-    if (timeout > 0) {
-      this.tokenRenewalTimeout = setTimeout(() => {
-        this.renewSession();
-      }, timeout);
     }
-  }
 
-  getExpiryDate() {
-    return JSON.stringify(new Date(this.expiresAt));
-  }
+    renewSession() {
+        this.auth0.checkSession({}, (err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                this.setSession(authResult);
+            } else if (err) {
+                this.logout();
+                console.log(err);
+                alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+            }
+        });
+    }
 
-  logout() {
-    // Remove tokens and expiry time
-    this.accessToken = null;
-    this.idToken = null;
-    this.expiresAt = 0;
+    getProfile(cb) {
+        this.auth0.client.userInfo(this.accessToken, (err, profile) => {
+            if (profile) {
+                this.userProfile = profile;
+            }
+            cb(err, profile);
+        });
+    }
 
-    // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
+    logout() {
+        // Remove tokens and expiry time
+        this.accessToken = null;
+        this.idToken = null;
+        this.expiresAt = 0;
 
-    // Clear token renewal
-    clearTimeout(this.tokenRenewalTimeout);
+        // Remove user profile
+        this.userProfile = null;
 
-    // navigate to the home route
-    history.replace('/');
-  }
+        // Clear token renewal
+        clearTimeout(this.tokenRenewalTimeout);
 
-  isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    let expiresAt = this.expiresAt;
-    return new Date().getTime() < expiresAt;
-  }
+        // Remove isLoggedIn flag from localStorage
+        localStorage.removeItem('isLoggedIn');
 
-  login() {
-    this.auth0.authorize();
-  }
+        // navigate to the home route
+        history.replace('/home');
+    }
+
+    isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        let expiresAt = this.expiresAt;
+        return new Date().getTime() < expiresAt;
+    }
+
+    scheduleRenewal() {
+        let expiresAt = this.expiresAt;
+        const timeout = expiresAt - Date.now();
+        if (timeout > 0) {
+            this.tokenRenewalTimeout = setTimeout(() => {
+            this.renewSession();
+            }, timeout);
+        }
+    }
+
+    getExpiryDate() {
+        return JSON.stringify(new Date(this.expiresAt));
+    }
 }
---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'familink.auth0.com',
+    clientId: 'uy5FOqsRDTpLBj20zosqyBB16Cr9Mmvr',
+    callbackUrl: 'http://localhost:3000/callback'
+}
