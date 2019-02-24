fb6a16d47a6aec77b89c2f39ffc1d672d1ae8cd8
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-02-24 14:21:31 -0600

    Update dashboard.jsx

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -6,68 +6,72 @@ import TableList from "views/TableList/TableList.jsx";
 import Maps from "views/Maps/Maps.jsx";
 import UserPage from "views/UserPage/UserPage.jsx";
 
-var dashRoutes = [
-  {
-    path: "/dashboard/user-page",
-    name: "Profile",
-    icon: "nc-icon nc-single-02",
-    component: UserPage
-  },
-  {
-    path: "/dashboard/notifications",
-    name: "Notifications",
-    icon: "nc-icon nc-bell-55",
-    component: Notifications
-  },
-  {
-    path: "/dashboard/patient-demo",
-    name: "Patient Demo",
-    icon: "nc-icon nc-alert-circle-i",
-    component: Dashboard
-  },
-  {
-    path: "/dashboard/doctor-demo",
-    name: "Doctor Demo",
-    icon: "nc-icon nc-ambulance",
-    component: Dashboard
-  },
+import DoctorDemo from "views/DoctorDemo/DoctorDemo";
+import PatientDemo from "views/PatientDemo/PatientDemo";
 
-  // {
-  //   path: "/dashboard/dashboard",
-  //   name: "Dashboard",
-  //   icon: "nc-icon nc-bank",
-  //   component: Dashboard
-  // },
-  {
-    path: "/dashboard/icons",
-    name: "Icons",
-    icon: "nc-icon nc-diamond",
-    component: Icons
-  },
-  // { path: "/dashboard/maps", name: "Maps", icon: "nc-icon nc-pin-3", component: Maps },
-  // {
-  //   path: "/dashboard/notifications",
-  //   name: "Notifications",
-  //   icon: "nc-icon nc-bell-55",
-  //   component: Notifications
-  // },
-  // {
-  //   path: "/dashboard/user-page",
-  //   name: "User Profile",
-  //   icon: "nc-icon nc-single-02",
-  //   component: UserPage
-  // },
-  {
-    path: "/dashboard/tables",
-    name: "Table List",
-    icon: "nc-icon nc-tile-56",
-    component: TableList
-  },
-  // {
-  //   path: "/dashboard/typography",
-  //   name: "Typography",
-  //   icon: "nc-icon nc-caps-small",
-  //   component: Typography
-  // },
+
+var dashRoutes = [
+    {
+        path: "/dashboard/user-page",
+        name: "Profile",
+        icon: "nc-icon nc-single-02",
+        component: UserPage
+    },
+    {
+        path: "/dashboard/notifications",
+        name: "Notifications",
+        icon: "nc-icon nc-bell-55",
+        component: Notifications
+    },
+    {
+        path: "/dashboard/patient-demo",
+        name: "Patient Demo",
+        icon: "nc-icon nc-alert-circle-i",
+        component: PatientDemo
+    },
+    {
+        path: "/dashboard/doctor-demo",
+        name: "Doctor Demo",
+        icon: "nc-icon nc-ambulance",
+        component: DoctorDemo
+    },
+
+    // {
+    //   path: "/dashboard/dashboard",
+    //   name: "Dashboard",
+    //   icon: "nc-icon nc-bank",
+    //   component: Dashboard
+    // },
+    {
+        path: "/dashboard/icons",
+        name: "Icons",
+        icon: "nc-icon nc-diamond",
+        component: Icons
+    },
+    // { path: "/dashboard/maps", name: "Maps", icon: "nc-icon nc-pin-3", component: Maps },
+    // {
+    //   path: "/dashboard/notifications",
+    //   name: "Notifications",
+    //   icon: "nc-icon nc-bell-55",
+    //   component: Notifications
+    // },
+    // {
+    //   path: "/dashboard/user-page",
+    //   name: "User Profile",
+    //   icon: "nc-icon nc-single-02",
+    //   component: UserPage
+    // },
+    {
+        path: "/dashboard/tables",
+        name: "Table List",
+        icon: "nc-icon nc-tile-56",
+        component: TableList
+    },
+    // {
+    //   path: "/dashboard/typography",
+    //   name: "Typography",
+    //   icon: "nc-icon nc-caps-small",
+    //   component: Typography
+    // },
 ];
 export default dashRoutes;
